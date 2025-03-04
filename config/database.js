d2d25e0051fd629b9a4eb50c4dbd8bcafacc1aee
Oleksandr-Nyk <201734733+Oleksandr-Nyk@users.noreply.github.com>
2025-03-04 19:31:27 +0200

    Updated database connection file

--- config/database.js
+++ config/database.js
@@ -2,14 +2,16 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: process.env.DATABASE_CLIENT,
     connection: {
-      host: env('DATABASE_HOST', 'db.jsgkncdrhlydwikdqenp.supabase.co'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'was328-Age46'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST', process.env.SUPABASE_HOST),
+      port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+      ssl: {
+        rejectUnauthorized: process.env.DATABASE_SSL,
+      },
     },
     useNullAsDefault: true,
   }
